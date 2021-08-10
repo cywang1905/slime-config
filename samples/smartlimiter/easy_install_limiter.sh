3c56baac93bb3c063302a754ab00581aa7a962c0
wangchenyu07 <wangchenyu07@corp.netease.com>
2021-08-10 11:06:10 +0800

    bug fix

--- samples/smartlimiter/easy_install_limiter.sh
+++ samples/smartlimiter/easy_install_limiter.sh
@@ -2,4 +2,4 @@
 kubectl create ns mesh-operator
 kubectl apply -f https://raw.githubusercontent.com/cywang1905/slime-config/main/init/crds.yaml
 kubectl apply -f https://raw.githubusercontent.com/cywang1905/slime-config/main/init/slime-boot-install.yaml
-kubectl apply -f https://raw.githubusercontent.com/cywang1905/slime-config/main/samples/lazyload/easy_install_limiter.yaml
+kubectl apply -f https://raw.githubusercontent.com/cywang1905/slime-config/main/samples/smartlimiter/easy_install_limiter.yaml
